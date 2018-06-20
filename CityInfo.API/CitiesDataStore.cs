db5873d7641045e432694d75b95be905838c5de1
digoumenidis <digoumenidis@egritosgroup.gr>
2018-06-20 16:33:54 +0300

    cities

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,59 @@
+﻿using CityInfo.API.Controllers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "new york",
+                    Description = "amerika fuck yeah",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "the most gamaoua"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Central Perk",
+                            Description = "auto apo ta filarakia"
+                        },
+
+                    }
+                },
+                new CityDto()
+                {
+                Id = 2,
+                Name = "new york",
+                Description = "amerika fuck yeah"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "new york",
+                    Description = "amerika fuck yeah"
+                }
+
+            };
+        }
+    
+     }       
+}
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -11,24 +11,53 @@ namespace CityInfo.API
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) 
             {
                 app.UseDeveloperExceptionPage(); //middleware
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler();
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
+
+
+
+
+
+
+            //app.Run((context) =>
+            //{
+            //    throw new Exception("example");
+            //});
+
+
+            //app.Run(async (context) =>
+            //{
+            //    if (context.Request.Path.ToString().Contains("mitsos"))
+            //    {
+            //        await context.Response.WriteAsync("Hello mitsos!");
+            //    }
+            //    else { 
+            //        await context.Response.WriteAsync("Hello World!");
+            //    }
+            //});
         }
+
     }
 }
